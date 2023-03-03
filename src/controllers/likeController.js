fbee63134498bb0c949e280b614c7118093f5954
hoanghai <hainguyenlehoang86@gmail.com>
2023-03-03 17:36:54 +0700

    ''

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,17 +1,84 @@
 const { successCode, errorCode } = require("../config/response");
 const initModel = require("../models/init-models");
 const sequelize = require("../models/index");
+const moment = require("moment/moment");
 const model = initModel(sequelize);
 
+
 //XỬ LÝ LIKE NHÀ HÀNG
-const createLike = (req,res) =>{
-    res.status(200).send("Create like")
+const createLike = async (req, res) => {
+    // res.status(200).send("Create like")
+    try {
+        //Lấy từ FE
+        let { user_id, res_id } = req.body;
+        //yarn add moment
+        // let dateString = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+        // let date_like = dateString;
+        
+        let date_like = new Date();
+        console.log(date_like);
+        console.log(typeof date_like)
+        let data = {
+            user_id,
+            res_id,
+            date_like
+        }
+
+        //Thêm 1 dòng dữ liệu vào table
+        await model.like_res.create(modelLike);
+        successCode(res, data, "Like thành công!")
+
+    } catch (error) {
+        errorCode(res, "Lỗi BE hoặc user_id và res_id đã tồn tại!")
+    }
 }
 
 //XỬ LÝ UNLIKE NHÀ HÀNG
+const deleteLike = async (req, res) => {
+    try {
+        //Lấy từ FE
+        let { user_id, res_id } = req.params;
+        let data = {
+            user_id,
+            res_id,
+        }
+
+        //Xóa 1 dòng dữ liệu trên table
+        await model.like_res.destroy({
+            where: data
+        });
+
+        // let data = await model.like_res.destroy({
+        //     where: {
+        //         user_id: req.params.user_id,
+        //         res_id: req.params.res_id
+        //     }
+        // })
+
+        successCode(res, data, "Unlike thành công!")
+
+    } catch (error) {
+        errorCode(res, "Lỗi BE hoặc kiểm tra lại user_id và res_id xem truyền vào đúng không!")
+    }
+}
 
 //LẤY DANH SÁCH LIKE THEO NHÀ HÀNG VÀ USER
+const getLike = async (req, res) => {
+    try {
+        //Lấy từ FE
+        let data = await model.like_res.findAll({
+            include: ["re", "user"]
+        })
+
+        successCode(res, data, "Lấy danh sách like theo nhà hàng và user thành công!")
+        
+    } catch (error) {
+        errorCode(res, "Lỗi BE!")
+    }
+}
 
 module.exports = {
     createLike,
+    deleteLike,
+    getLike
 }
--- src/routers/likeRoute.js
+++ src/routers/likeRoute.js
@@ -2,9 +2,16 @@ const express = require("express");
 const likeRoute = express.Router();
 
 //import controller
-const { createLike } = require("../controllers/likeController");
+const { createLike, deleteLike, getLike } = require("../controllers/likeController");
+
 
 //POST
 likeRoute.post("/createLike", createLike)
+
+//DELETE
+likeRoute.delete("/deleteLike/:user_id/:res_id", deleteLike)
+
+//GET
+likeRoute.get("/getLike", getLike)
 
 module.exports = likeRoute;
